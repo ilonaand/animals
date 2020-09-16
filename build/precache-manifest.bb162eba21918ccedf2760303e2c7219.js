4076e0b0ebfe8f8ce0c4c42ce3f3ce4b002d86ae
ilona <ilonaand@gmail.com>
2020-09-16 14:07:33 +0300

    1

---
+++ build/precache-manifest.bb162eba21918ccedf2760303e2c7219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aba3128398441f0982dab4bfe6e1581",
+    "url": "/animals/build/index.html"
+  },
+  {
+    "revision": "dc4ea6d1564d8552b093",
+    "url": "/animals/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "316125f0fdde2035028f",
+    "url": "/animals/build/static/js/2.40f711df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animals/build/static/js/2.40f711df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4ea6d1564d8552b093",
+    "url": "/animals/build/static/js/main.47cdcf0a.chunk.js"
+  },
+  {
+    "revision": "ec826d84507c42e8769f",
+    "url": "/animals/build/static/js/runtime-main.cb0b91a6.js"
+  }
+]);
